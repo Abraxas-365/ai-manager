dc179fd0cc7e27aaae9a5aa970bd7b6f7b79528f
Luis.Fernando <luisfmiranda8@gmail.com>
2023-04-12 18:28:04 -0500

    adding request prompt planners

--- pkg/openapi/plannerPrompt.go
+++ pkg/openapi/plannerPrompt.go
@@ -1,6 +1,10 @@
 package openapi
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/Abraxas-365/ai-manager/pkg/prompt"
+)
 
 var (
 	ApiPlannerPrompt = `You are a planner that plans a sequence of API calls to assist with user queries against an API.
@@ -62,4 +66,115 @@ Plan:`
 	ApiPlannerToolName = "api_planner"
 
 	ApiPlannerToolDescription = fmt.Sprintf("can be used to generate the right api calls to assist with a user query, like %s(query). Should always be called before trying to call the API controller.", ApiPlannerToolName)
+
+	ApiControllerToolName = "api_controller"
+
+	ApiControllerToolDescription = fmt.Sprintf("can be used to execute a plan of api calls, like %s(plan).", ApiControllerToolName)
+
+	ApiOrchestratorPrompt = `
+You are an agent that assists with user queries against API, things like querying information or creating resources.
+Some user queries can be resolved in a single API call, particularly if you can find appropriate params from the OpenAPI spec; though some require several API call.
+You should always plan your API calls first, and then execute the plan second.
+If the plan includes a DELETE call, be sure to ask the User for authorization first unless the User has specifically asked to delete something.
+You should never return information without executing the api_controller tool.
+
+
+Here are the tools to plan and execute API requests: {{.tool_descriptions}}
+
+
+Starting below, you should follow this format:
+
+User query: the query a User wants help with related to the API
+Thought: you should always think about what to do
+Action: the action to take, should be one of the tools [{{.tool_names}}]
+Action Input: the input to the action
+Observation: the result of the action
+... (this Thought/Action/Action Input/Observation can repeat N times)
+Thought: I am finished executing a plan and have the information the user asked for or the data the used asked to create
+Final Answer: the final output from executing the plan
+
+
+Example:
+User query: can you add some trendy stuff to my shopping cart.
+Thought: I should plan API calls first.
+Action: api_planner
+Action Input: I need to find the right API calls to add trendy items to the users shopping cart
+Observation: 1) GET /items with params 'trending' is 'True' to get trending item ids
+2) GET /user to get user
+3) POST /cart to post the trending items to the user's cart
+Thought: I'm ready to execute the API calls.
+Action: api_controller
+Action Input: 1) GET /items params 'trending' is 'True' to get trending item ids
+2) GET /user to get user
+3) POST /cart to post the trending items to the user's cart
+...
+
+Begin!
+
+User query: {{.input}}
+Thought: I should generate a plan to help with this query and then copy that plan exactly to the controller.
+{{.agent_scratchpad}}
+`
+
+	RequestsGetToolDescription = `Use this to GET content from a website.
+Input to the tool should be a json string with 3 keys: "url", "params" and "output_instructions".
+The value of "url" should be a string. 
+The value of "params" should be a dict of the needed and available parameters from the OpenAPI spec related to the endpoint. 
+If parameters are not needed, or not available, leave it empty.
+The value of "output_instructions" should be instructions on what information to extract from the response, 
+for example the id(s) for a resource(s) that the GET request fetches.`
+
+	ParsingGetPrompt = prompt.NewPromptTemplateBuilder().
+				AddInputVariables([]string{"response", "instructions"}).
+				AddTemplate(`Here is an API response:\n\n{{.response}}\n\n====
+Your task is to extract some information according to these instructions: {{.instructions}}
+When working with API objects, you should usually use ids over names.
+If the response indicates an error, you should instead output a summary of the error.
+Output:`).
+		Build()
+
+	RequestsPostToolDescription = `Use this when you want to POST to a website.
+Input to the tool should be a json string with 3 keys: "url", "data", and "output_instructions".
+The value of "url" should be a string.
+The value of "data" should be a dictionary of key-value pairs you want to POST to the url.
+The value of "output_instructions" should be instructions on what information to extract from the response, for example the id(s) for a resource(s) that the POST request creates.
+Always use double quotes for strings in the json string.`
+
+	ParsingPostPrompt = prompt.NewPromptTemplateBuilder().
+				AddTemplate(`Here is an API response:\n\n{{.response}}\n\n====
+Your task is to extract some information according to these instructions: {{.instructions}}
+When working with API objects, you should usually use ids over names. Do not return any ids or names that are not in the response.
+If the response indicates an error, you should instead output a summary of the error.
+
+Output:`).
+		AddInputVariables([]string{"response", "instructions"}).Build()
+
+	RequestsPatchToolDescription = `use this when you want to patch content on a website.
+input to the tool should be a json string with 3 keys: "url", "data", and "output_instructions".
+the value of "url" should be a string.
+the value of "data" should be a dictionary of key-value pairs of the body params available in the openapi spec you want to patch the content with at the url.
+the value of "output_instructions" should be instructions on what information to extract from the response, for example the id(s) for a resource(s) that the patch request creates.
+always use double quotes for strings in the json string.`
+
+	ParsingPatchPrompt = prompt.NewPromptTemplateBuilder().
+				AddTemplate(`Here is an API response:\n\n{{.response}}\n\n====
+Your task is to extract some information according to these instructions: {{.instructions}}
+When working with API objects, you should usually use ids over names. Do not return any ids or names that are not in the response.
+If the response indicates an error, you should instead output a summary of the error.
+
+Output:`).AddInputVariables([]string{"response", "instructions"}).Build()
+
+	RequestsDeleteToolDescription = `ONLY USE THIS TOOL WHEN THE USER HAS SPECIFICALLY REQUESTED TO DELETE CONTENT FROM A WEBSITE.
+Input to the tool should be a json string with 2 keys: "url", and "output_instructions".
+The value of "url" should be a string.
+The value of "output_instructions" should be instructions on what information to extract from the response, for example the id(s) for a resource(s) that the DELETE request creates.
+Always use double quotes for strings in the json string.
+ONLY USE THIS TOOL IF THE USER HAS SPECIFICALLY REQUESTED TO DELETE SOMETHING.`
+
+	ParsingDeletePrompt = prompt.NewPromptTemplateBuilder().AddTemplate(`Here is an API response:\n\n{{.response}}\n\n====
+Your task is to extract some information according to these instructions: {{.instructions}}
+When working with API objects, you should usually use ids over names. Do not return any ids or names that are not in the response.
+If the response indicates an error, you should instead output a summary of the error.
+
+Output:`).AddInputVariables([]string{"response", "instructions"}).Build()
 )
