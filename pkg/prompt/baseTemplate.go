19e3393a402197a7730bf507850a3a062166671f
Luis.Fernando <luisfmiranda8@gmail.com>
2023-04-12 14:56:38 -0500

    adding builder

--- pkg/prompt/baseTemplate.go
+++ pkg/prompt/baseTemplate.go
@@ -15,6 +15,48 @@ type PromptTemplate struct {
 	PartialVariables map[string]interface{}
 }
 
+type PromptTemplateBuilder struct {
+	InputVariables   []string
+	Template         string
+	PartialVariables map[string]interface{}
+}
+
+type IPromptTemplateBuilder interface {
+	AddInputVariables([]string) IPromptTemplateBuilder
+	AddTemplate(string) IPromptTemplateBuilder
+	AddPartialVariables(map[string]interface{}) IPromptTemplateBuilder
+	Build() PromptTemplate
+}
+
+func NewPromptTemplateBuilder() *PromptTemplateBuilder {
+	return &PromptTemplateBuilder{}
+}
+func (b *PromptTemplateBuilder) AddInputVariables(inputVariables []string) IPromptTemplateBuilder {
+	b.InputVariables = inputVariables
+	return b
+}
+
+func (b *PromptTemplateBuilder) AddTemplate(template string) IPromptTemplateBuilder {
+	b.Template = template
+	return b
+}
+
+func (b *PromptTemplateBuilder) AddPartialVariables(partialVariables map[string]interface{}) IPromptTemplateBuilder {
+	b.PartialVariables = partialVariables
+	return b
+}
+
+func (b *PromptTemplateBuilder) Build() PromptTemplate {
+	return PromptTemplate{
+		InputVariables:   b.InputVariables,
+		Template:         b.Template,
+		TemplateFormat:   "f-string",
+		ValidateTemplate: true,
+		PartialVariables: b.PartialVariables,
+	}
+
+}
+
 func (p *PromptTemplate) Format(kwargs map[string]interface{}) (string, error) {
 	if p.TemplateFormat != "f-string" {
 		return "", fmt.Errorf("unsupported template format: %s", p.TemplateFormat)
@@ -40,22 +82,12 @@ func (p *PromptTemplate) Format(kwargs map[string]interface{}) (string, error) {
 	return sb.String(), nil
 }
 
-func NewPromptTemplate(inputVariables []string, tmpl string, partialVariables map[string]interface{}) *PromptTemplate {
-	return &PromptTemplate{
-		InputVariables:   inputVariables,
-		Template:         tmpl,
-		TemplateFormat:   "f-string",
-		ValidateTemplate: true,
-		PartialVariables: partialVariables,
-	}
-}
-
-func PromptTemplateFromFile(templateFile string, inputVariables []string, partialVariables map[string]interface{}) (*PromptTemplate, error) {
+func PromptTemplateFromFile(templateFile string, inputVariables []string, partialVariables map[string]interface{}) (PromptTemplate, error) {
 	templateBytes, err := ioutil.ReadFile(templateFile)
 	if err != nil {
-		return nil, err
+		return PromptTemplate{}, err
 	}
 
 	templateStr := string(templateBytes)
-	return NewPromptTemplate(inputVariables, templateStr, partialVariables), nil
+	return NewPromptTemplateBuilder().AddTemplate(templateStr).AddInputVariables(inputVariables).AddPartialVariables(partialVariables).Build(), nil
 }
